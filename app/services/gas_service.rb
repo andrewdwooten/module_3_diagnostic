aec1cc10a8dbcbfc074162c0fdc5bed7dcc8b690
Andy Wooten <andrewdwooten@icloud.com>
2017-03-29 09:52:25 -0600

    Response from request now appropriate

--- app/services/gas_service.rb
+++ app/services/gas_service.rb
@@ -1,14 +1,13 @@
 class GasService
   attr_reader :connection, :auth
-  
+
   def initialize
-    @connection = Faraday.new('https://api.data.gov/nrel/alt-fuel-stations/v1')
+    @connection = Faraday.new('https://api.data.gov')
     @auth = "api_key=#{ENV['GAS_KEY']}"
   end
 
   def stations_in_radius(zip_code)
-    response = connection.get "/nearest.json?location=#{zip_code}&radius=6&fuel_type=ELEC,LPG&limit=10&#{auth}"
-byebug
+    parse(connection.get "/nrel/alt-fuel-stations/v1/nearest.json?location=#{zip_code}&radius=6&fuel_type=ELEC,LPG&limit=10&#{auth}")
   end
 
 

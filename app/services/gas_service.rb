208df1be9bdf34609dabc342c7e6cd058d992575
Andy Wooten <andrewdwooten@icloud.com>
2017-03-29 10:01:09 -0600

    Implement cassette
    Add cassette for gas service test; same is
    passing appropriately; create PORO to
    format output data

--- app/services/gas_service.rb
+++ app/services/gas_service.rb
@@ -7,7 +7,11 @@ class GasService
   end
 
   def stations_in_radius(zip_code)
-    parse(connection.get "/nrel/alt-fuel-stations/v1/nearest.json?location=#{zip_code}&radius=6&fuel_type=ELEC,LPG&limit=10&#{auth}")
+  stations(parse(connection.get "/nrel/alt-fuel-stations/v1/nearest.json?location=#{zip_code}&radius=6&fuel_type=ELEC,LPG&limit=10&#{auth}"))
+  end
+
+  def stations(response)
+    response[:fuel_stations]
   end
 
 
--- spec/services/gas_service_spec.rb
+++ spec/services/gas_service_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 describe 'Gas Service' do
   describe '#stations_in_radius' do
     it 'returns the stations in a defined radius' do
+      VCR.use_cassette('services/gas_service/radius') do
       gas_service = GasService.new
       stations = gas_service.stations_in_radius('80205')
+      closest = stations.first
 
-      byebug
+      expect(closest[:station_phone]).to eq("888-758-4389")
+      expect(closest[:station_name]).to eq("ROCKIES PARKING")
+      expect(closest[:fuel_type_code]).to eq("ELEC")
+    end
     end
   end
 end
